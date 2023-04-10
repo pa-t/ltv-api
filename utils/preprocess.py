703c3453318686e48dd14e6e77c394535816b5a0
yemeen <yemeen.ayub.14@cnu.edu git config --global user.name yemeen>
2023-04-10 15:48:53 -0400

    fix preprocess_train and preprocess_predict

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -36,7 +36,7 @@ def calc_total_spent(dataset: pd.DataFrame) -> List[pd.Series]:
     filtered_dataset = dataset.loc[mask]
 
     # calculate the total amount spent by each customer
-    ltv = filtered_dataset.groupby('customer_id')['order_total'].sum().rename('ltv').astype(float)
+    ltv = filtered_dataset.groupby('customer_id')['order_total'].sum().rename(f'total_spent_{width}').astype(float)
     total_spent.append(ltv)
   
   return total_spent
@@ -71,12 +71,16 @@ def preprocess_train(dataset: pd.DataFrame, target_width: int) -> pd.DataFrame:
   """
     Series of preprocessing steps, takes raw data from db and prepares it for use
   """
-  # convert timestamp column to be datetime object
+  df = get_features(dataset)
+  targets_total_spent = calc_total_spent(dataset=dataset)
+  df = df.join(targets_total_spent)
+  return df
+
+
+def get_features(dataset:pd.DataFrame) -> pd.DataFrame:
   dataset['time_stamp'] = pd.to_datetime(dataset['time_stamp'])
   # filter out dirty data
   dataset = filter_data(dataset=dataset)
-  # calc total spents
-  targets_total_spent = calc_total_spent(dataset=dataset)
 
   # groupby customer id
   customer_data = dataset.groupby('customer_id')
@@ -91,23 +95,16 @@ def preprocess_train(dataset: pd.DataFrame, target_width: int) -> pd.DataFrame:
   afid = customer_data['afid'].first().astype(str)
   cc_type = customer_data['cc_type'].first().astype(str)
   main_product_id = customer_data['main_product_id'].first().astype(float)
-  version = customer_data['version'].first().astype(str)
   campaign_id = customer_data['campaign_id'].first().astype(float)
   domain = customer_data['email_address'].first().str.split('@').str[1].astype(str)
-  first_on_hold = customer_data['on_hold'].first()
-
+ 
   df = pd.DataFrame({
     'afid': afid.astype(str),
     'cc_type': cc_type.astype(str),
     'main_product_id': main_product_id.astype(int),
     'campaign_id': campaign_id.astype(int),
     'first_order_amount': first_order_amount.astype(float),
-    'domain': domain.astype(str),
-    'total_spent_30': targets_total_spent[0].astype(float),
-    'total_spent_90': targets_total_spent[1].astype(float),
-    'total_spent_365': targets_total_spent[2].astype(float),
-    'version': version.astype(str),
-    'first_on_hold': first_on_hold.astype(str)
+    'domain': domain.astype(str)
   })
 
   # drop the overspending accounts
@@ -121,32 +118,15 @@ def preprocess_train(dataset: pd.DataFrame, target_width: int) -> pd.DataFrame:
   customer_window = dataset.groupby('customer_id')['time_stamp'].first() > time_cutoff
   df.drop(df[customer_window].index)
 
-  # Add affiliate id and their respective funnel domain (google, bing, email, etc)
-  afid_df = pd.read_csv('./static_data/afid_mapping_ath.csv')
-  df = pd.merge(df, afid_df, on='afid', how='left').set_index(df.index)
-  top_sources = df['source_system_description'].value_counts().nlargest(10).index
-  df['source_system_description'] = np.where(df['source_system_description'].isin(top_sources),df['source_system_description'], 'Other')
-
-  # Restrict the afid column to the top 20 values or 'other'
-  top_afid = df['afid'].value_counts().nlargest(20).index
-  df['afid'] = np.where(df['afid'].isin(top_afid), df['afid'], 'other')
-
-  # add in billing state column
-  # TODO: figure out where to get this csv from
-  # billing_state = pd.read_csv('./drive/MyDrive/ath_billing_state.csv')
-  # billing_state = billing_state.set_index('customer_id')
-  # billing_state = billing_state.groupby('customer_id').first()
-  # df = df.merge(billing_state, on='customer_id')
 
   # fill na values
   df = df.fillna('Other')
 
   return df
-
-
+  
 def preprocess_predict(dataset: pd.DataFrame):
-  # TODO: implement this preprocessing
-  return dataset
+  df = get_features(dataset)
+  return df
 
 def check_columns(df: DataFrame):
     required_columns = [
@@ -158,5 +138,4 @@ def check_columns(df: DataFrame):
 
     if missing_columns:
         return True
-    else:
-        return False
+    return False
