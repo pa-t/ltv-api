196cba21747ab8b95d301a0766d4e2f8c5f1be17
yemeen <yemeen.ayub.14@cnu.edu git config --global user.name yemeen>
2023-04-13 18:52:05 -0400

    added model compile

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import logging
+import pickle
 import pandas as pd
 
 from fastapi import FastAPI, File, UploadFile
 from io import BytesIO
 from keras.models import load_model, save_model
 from starlette.responses import JSONResponse
+import tensorflow as tf
 
 from domain.enums import ModelTimeFrame
-from utils.preprocess import preprocess_train, preprocess_predict, check_columns
+from domain.exceptions import MissingColumnsException
+from utils.preprocess import preprocess_train, get_features, check_columns
+from utils.zltv_model import model_predict, zero_inflated_lognormal_loss
+
 
 app = FastAPI()
+
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 
 @app.post("/model/predict")
-def predict(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = ModelTimeFrame.month):
+def predict(file: UploadFile = File(...), model_time_frame: ModelTimeFrame=ModelTimeFrame.month):
   try:
      # read file contents into a BytesIO stream
     contents = file.file.read()
@@ -27,33 +33,30 @@ def predict(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = Mod
     file.file.close()
 
     # convert historical transaction data into customer profile
-    df = preprocess_predict(dataset=df)
+    df = get_features(dataset=df, target_width=int(model_time_frame.value))
 
-    # TODO: does this need to change bc of diff between sagemaker / our new pipeline
     # drop target column and select which model to use
     if model_time_frame.value == "365":
       model_path = 'models/year/365model.h5'
       logger.info("Using year ltv model...")
-      df = df.drop(columns=['total_spent_365'])
     elif model_time_frame.value == "90":
       model_path = 'models/quarter/90model.h5'
       logger.info("Using quarter ltv model...")
-      df = df.drop(columns=['total_spent_90'])
     else:
       model_path = 'models/month/30model.h5'
       logger.info("Using month ltv model...")
-      df = df.drop(columns=['total_spent_30'])
-    
+
     # use keras to load in the correct file
-    model = load_model(model_path)
+    model = load_model(model_path, compile=False, custom_objects={'zero_inflated_lognormal_loss': zero_inflated_lognormal_loss})
+    model.compile(loss=zero_inflated_lognormal_loss)
+    with open('static_data/feature_map.pkl', 'rb') as f:
+      feature_map = pickle.load(f)
     
-    # TODO: does this work?
-    import pdb;pdb.set_trace()
     # call predict on the model and get the results
-    results = model.predict(df)
+    results = model_predict(model, data=df, feature_map=feature_map)
     
     # construct dataframe with predicted ltv married to customer id
-    results_df = pd.DataFrame({'customer_id': df.customer_id, 'predicted_ltv': results})
+    results_df = pd.DataFrame({'customer_id': df.index, 'predicted_ltv': results.ltv_prediction})
 
     return JSONResponse(
       content={'predictions': results_df.to_dict(orient='records')}
@@ -65,8 +68,7 @@ def predict(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = Mod
 
 
 @app.post("/model/train")
-# TODO: make this default to month
-def train(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = ModelTimeFrame.year):
+def train(file: UploadFile = File(...), model_time_frame: ModelTimeFrame=ModelTimeFrame.month):
   try:
     # read file contents into a BytesIO stream
     contents = file.file.read()
@@ -82,7 +84,7 @@ def train(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = Model
       raise MissingColumnsException(missing_columns)
     else:
       logger.info("All required columns are present in the DataFrame")
-      df = preprocess_train(dataset=df, target_width=int(model_time_frame.value))
+      x_train, y_train = preprocess_train(dataset=df, target_width=int(model_time_frame.value))
 
       if model_time_frame.value == "365":
         model_path = 'models/year/365model.h5'
@@ -93,13 +95,26 @@ def train(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = Model
       else:
         model_path = 'models/month/30model.h5'
         logger.info("Using month ltv model...")
-      
 
       # use keras to load in the correct file
-      model = load_model(model_path)
+      model = load_model(model_path, compile=False, custom_objects={'zero_inflated_lognormal_loss': zero_inflated_lognormal_loss})
+      model.compile(loss=zero_inflated_lognormal_loss)
       
       # fit the model to the newly given data
-      model.fit(df)
+      callback_patience = 5
+      epochs = 100
+      callbacks = [
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=callback_patience)
+      ]
+
+      model.fit(
+        x_train,
+        y_train,
+        batch_size=1024,
+        epochs=epochs,
+        verbose=1,
+        callbacks=callbacks
+      )
 
       # write the model back to the path
       save_model(model=model, filepath=model_path)
@@ -113,7 +128,5 @@ def train(file: UploadFile = File(...), model_time_frame: ModelTimeFrame = Model
     ERR_MSG = f'Exception encountered training model: {e}'
     logger.error(ERR_MSG)
     return JSONResponse(content={'status': ERR_MSG}, status_code=500)
-class MissingColumnsException(Exception):
-    def __init__(self, missing_columns):
-        super().__init__(f"Missing columns: {', '.join(missing_columns)}")
-        self.missing_columns = missing_columns
+
+
